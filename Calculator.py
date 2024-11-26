c7b8988a3a5811d753be56d812abfd49c2a40f64
Bluuwaves <hiyah473@gmail.com>
2024-11-26 13:23:40 -0500

    Add files via upload
    Mr.Jean Calculator

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+x=5
+y=9
+
+addition = x+y 
+
+print(addition)
+
+subtraction = x-y
+
+print(subtraction)
+
+multiplcation = x*y
+
+print(multiplcation)
+
+division = x/y
+
+print(division)
+
+power = x**y 
+
+print(power)
+
